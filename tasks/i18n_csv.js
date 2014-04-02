012dba70951b537ead5b59efb547d58fdc4c14ad
Laurent Goderre <laurent.goderre@gmail.com>
2014-04-02 12:54:51 -0400

    Added the skeleton for the plugin generated by Yeoman

---
+++ tasks/i18n_csv.js
@@ -0,0 +1,51 @@
+/*
+ * grunt-i18n-csv
+ * https://github.com/wet-boew/grunt-i18n-csv
+ *
+ * Copyright (c) 2014 Laurent Goderre
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function (grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('i18n_csv', 'Create internationalized files from a CSV translations file', function () {
+
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      punctuation: '.',
+      separator: ', '
+    });
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function (file) {
+      // Concat specified files.
+      var src = file.src.filter(function (filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function (filepath) {
+        // Read file source.
+        return grunt.file.read(filepath);
+      }).join(grunt.util.normalizelf(options.separator));
+
+      // Handle options.
+      src += options.punctuation;
+
+      // Write the destination file.
+      grunt.file.write(file.dest, src);
+
+      // Print a success message.
+      grunt.log.writeln('File "' + file.dest + '" created.');
+    });
+  });
+
+};
